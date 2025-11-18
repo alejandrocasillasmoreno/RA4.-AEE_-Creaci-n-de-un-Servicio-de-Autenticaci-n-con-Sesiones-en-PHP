408cd71e9212f6b80c9867ca016549d5c399460a
alejandrocasillasmoreno <alejandrocasillas.24@campuscamara.es>
2025-11-18 11:41:33 +0100

    feat: Mejora de codigo

--- Formulario.php
+++ Formulario.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Una pantalla de login que usaremos para autenticar a los usuarios">
         <title>Pantalla de login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
         <div class="container mt-4">
